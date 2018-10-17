2b192461e72a07fc295f13e0dc0a2734b4c0d5c0
enri10 <43589316+enri10@users.noreply.github.com>
2018-10-17 01:14:35 -0700

    .

--- TIVAC/LAB03/code/task01.c
+++ TIVAC/LAB03/code/task01.c
@@ -1,3 +1,6 @@
+//Enrique Saldana
+//CpE 403
+//Lab 3 Task 01
 #include <stdint.h>
 #include <stdbool.h>
 #include "inc/hw_memmap.h"
@@ -9,17 +12,25 @@ uint8_t ui8PinData=2;
 
 int main(void)
 {
-	SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+    //Set clock to 4.7 MHz
+	SysCtlClockSet(SYSCTL_SYSDIV_42_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 
+	//Enable clock for peripheral
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+	//Configure LED's as outputs
 	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
 
 	while(1)
 	{
+	    //Turn on LED
 		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, ui8PinData);
 		SysCtlDelay(2000000);
+		//Turn off LED
 		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x00);
 		SysCtlDelay(2000000);
+		//Cycle through R, B, G, R, B, G ...
 		if(ui8PinData==8) {ui8PinData=2;} else {ui8PinData=ui8PinData*2;}
 	}
 }
+
+
