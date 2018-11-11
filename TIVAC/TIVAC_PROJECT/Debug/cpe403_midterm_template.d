f0a558e5550c8838a231de199b5a72cb455c4b0a
enri10 <43589316+enri10@users.noreply.github.com>
2018-11-10 16:56:23 -0800

    .

---
+++ TIVAC/TIVAC_PROJECT/Debug/cpe403_midterm_template.d
@@ -0,0 +1,63 @@
+# FIXED
+
+cpe403_midterm_template.obj: ../cpe403_midterm_template.c
+cpe403_midterm_template.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdarg.h
+cpe403_midterm_template.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h
+cpe403_midterm_template.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h
+cpe403_midterm_template.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h
+cpe403_midterm_template.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdbool.h
+cpe403_midterm_template.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h
+cpe403_midterm_template.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h
+cpe403_midterm_template.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h
+cpe403_midterm_template.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h
+cpe403_midterm_template.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h
+cpe403_midterm_template.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_i2c.h
+cpe403_midterm_template.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+cpe403_midterm_template.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+cpe403_midterm_template.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h
+cpe403_midterm_template.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/i2c.h
+cpe403_midterm_template.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+cpe403_midterm_template.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+cpe403_midterm_template.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+cpe403_midterm_template.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
+cpe403_midterm_template.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h
+cpe403_midterm_template.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+cpe403_midterm_template.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/hibernate.h
+cpe403_midterm_template.obj: ../TSL2591_def.h
+cpe403_midterm_template.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h
+cpe403_midterm_template.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/time.h
+cpe403_midterm_template.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/_ti_config.h
+cpe403_midterm_template.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/linkage.h
+cpe403_midterm_template.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/abi_prefix.h
+cpe403_midterm_template.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/xlocale/_time.h
+
+../cpe403_midterm_template.c: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdarg.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdbool.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_i2c.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/i2c.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/hibernate.h: 
+../TSL2591_def.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/time.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/_ti_config.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/linkage.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/abi_prefix.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/xlocale/_time.h: 
